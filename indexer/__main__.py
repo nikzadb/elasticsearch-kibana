9a4820fb7ff3cb5a10b9a7971030fba25340a3c8
Nikzad <nikzad.b@gmail.com>
2020-08-13 13:10:15 +1000

    Kinda working version

--- indexer/__main__.py
+++ indexer/__main__.py
@@ -1,19 +1,36 @@
+import elasticsearch
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
+from textblob import TextBlob
+import pandas as pd
+import time
 
-es = Elasticsearch(hosts=[{"host":'elasticsearch'}]) # what should I put here?
+# es = Elasticsearch(hosts=[{"host":'elasticsearch'}]) # what should I put here?
+es = Elasticsearch(hosts="http://elastic:changeme@elasticsearch:9200/")
 
-actions = [
-    {
-    '_index' : 'test',
-    '_type' : 'content',
-    '_id' : str(item['id']),
-    '_source' : item,
-    }
-for item in [{'id': 1, 'foo': 'bar'}, {'id': 2, 'foo': 'spam'}]
-]
+df = pd.read_csv('data/BBC-News-Train.csv', sep=',', usecols=['Text'])
+text = TextBlob(df.to_string())
+
+def index_text():
+    for idx, txt in enumerate(text):
+        if idx % 1000 == 0:
+            print(f"----> {idx} <---")
+        es.index(index="text",
+                 doc_type="test-type",
+                 body={"body_text":    txt,
+                       "polarity":     text.sentiment.polarity,
+                       "subjectivity": text.sentiment.subjectivity})
 
 # create index
-print("Indexing Elasticsearch db... (please hold on)")
-bulk(es, actions)
-print("...done indexing :-)")
+while True:
+    try:
+        print("Indexing Elasticsearch db... (please hold on)")
+        index_text()
+        print("...done indexing :-)")
+        break
+    except (
+        elasticsearch.exceptions.ConnectionError,
+        elasticsearch.exceptions.TransportError
+    ):
+        print("---> ES connection is down <--")
+        time.sleep(5)
